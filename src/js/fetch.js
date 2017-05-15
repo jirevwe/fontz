c3afb3bca5e98dc215b442c97f92ae7d53f213e4
Raymond Tukpe <rtukpe@gmail.com>
2017-05-15 05:31:36 +0100

    done loading of font options

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -22,67 +22,95 @@
     let option_3 = document.getElementById('three');
     let option_4 = document.getElementById('four');
 
-    // let current = [];
-    // while(current.length < 4){
-    //     current.push(fonts[Object.keys(fonts).randomElement()]);
-    // }
+    let options = [option_1, option_2, option_3, option_4];
+    let current = [];
+    let opts = ['0','1','2','3'];
 
-    question.style.fontFamily = fonts[Object.keys(fonts).randomElement()];
-
-    option_1.innerHTML = fonts[Object.keys(fonts).randomElement()];
-    option_2.innerHTML = fonts[Object.keys(fonts).randomElement()];
-    option_3.innerHTML = fonts[Object.keys(fonts).randomElement()];
-    option_4.innerHTML = fonts[Object.keys(fonts).randomElement()];
-
-    document.getElementById('butRefresh').addEventListener('click', () => {
-        question.style.fontFamily = fonts[Object.keys(fonts).randomElement()];
-
-        option_1.innerHTML = fonts[Object.keys(fonts).randomElement()];
-        option_2.innerHTML = fonts[Object.keys(fonts).randomElement()];
-        option_3.innerHTML = fonts[Object.keys(fonts).randomElement()];
-        option_4.innerHTML = fonts[Object.keys(fonts).randomElement()];
-    });
-})();
-
-let fonts = {
-    'roboto': 'Roboto',
-    'open-sans': 'Open Sans',
-    'droid-serif': 'Droid Serif',
-    'indie-flower': 'Indie Flower',
-    'lobster': 'Lobster',
-    'pacifico': 'Pacifico',
-    'archivo-black': 'Archivo Black',
-    'hammersmith-one': 'Hammersmith One',
-    'poiret-one': 'Poiret One'
-};
-
-let getStyleRuleValue = function (style, selector, sheet) {
-    var sheets = typeof sheet !== 'undefined' ? [sheet] : document.styleSheets;
-    for (var i = 0, l = sheets.length; i < l; i++) {
-        var sheet = sheets[i];
-        if (!sheet.cssRules) { continue; }
-        for (var j = 0, k = sheet.cssRules.length; j < k; j++) {
-            var rule = sheet.cssRules[j];
-            if (rule.selectorText && rule.selectorText.split(',').indexOf(selector) !== -1) {
-                return rule.style[style];
+    for(let i = 0;i < 4;i++){
+        let font = fonts[Object.keys(fonts).randomElement()];
+        while (true){
+            if (current.indexOf(font) == -1){
+                current.push(font);
+                break;
+            }else{
+                font = fonts[Object.keys(fonts).randomElement()];
             }
         }
     }
-    return null;
-}
+
+    let next = opts.randomElement();
+    let index = opts.indexOf(next);
+    opts.splice(index, 1);
+
+    question.style.fontFamily = current[next];
+    option_1.innerHTML = current[next];
+
+    next = opts.randomElement();
+    index = opts.indexOf(next);
+    opts.splice(index, 1);
+
+    option_2.innerHTML = current[next];
+
+    next = opts.randomElement();
+    index = opts.indexOf(next);
+    opts.splice(index, 1);
+
+    option_3.innerHTML = current[next];
+
+    next = opts.randomElement();
+    index = opts.indexOf(next);
+    opts.splice(index, 1);
+    
+    option_4.innerHTML = current[next];
+
+    document.getElementById('butRefresh').addEventListener('click', () => {
+        let options = [option_1, option_2, option_3, option_4];
+        let current = [];
+        let opts = [0,1,2,3];
+
+        for(let i = 0;i < 4;i++){
+            let font = fonts[Object.keys(fonts).randomElement()];
+            while (true){
+                if (current.indexOf(font) == -1){
+                    current.push(font);
+                    break;
+                }else{
+                    font = fonts[Object.keys(fonts).randomElement()];
+                }
+            }
+        }
+
+        let next = opts.randomElement();
+        let index = opts.indexOf(next);
+        opts.splice(index);
+        question.style.fontFamily = current[next];
+        option_1.innerHTML = next;
+
+        next = opts.randomElement();
+        index = opts.indexOf(next);
+        opts.splice(index);
+        option_2.innerHTML = next;
+
+        next = opts.randomElement();
+        index = opts.indexOf(next);
+        opts.splice(index);
+        option_3.innerHTML = next;
+
+        next = opts.randomElement();
+        index = opts.indexOf(next);
+        opts.splice(index);
+        option_4.innerHTML = next;
+    });
+})();
 
 let getColor = function (id) {
     let element = document.getElementById(id);
-    // console.log(element.classList[1]);
     let text = document.getElementById('question');
-    console.log(element.innerHTML);
+
     if(text.style.fontFamily.replace(/"/g, '') === element.innerHTML)
     {
         toast('Correct', 1500);
-        // location = 'index.html';
     }else{
         toast('wrong', 1500);
     }
-    // console.log(text.style.fontFamily.replace('"', '').replace('"', ''), fonts[element.classList[1]]);
-    // console.log(text.style.fontFamily.replace('"', '').replace('"', '') === fonts[element.classList[1]]);
 }
